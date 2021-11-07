37dceae10188fea248aa4a7b88c5ffc7d8f0ddbf
xoxoedg <rugyasin@gmail.com>
2021-11-07 14:47:29 +0100

    Initial Commit

---
+++ src/main/java/com/example/rug/RugApplication.java
@@ -0,0 +1,13 @@
+package com.example.rug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rug/RugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
