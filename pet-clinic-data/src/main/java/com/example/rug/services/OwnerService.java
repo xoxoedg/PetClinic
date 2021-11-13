8140d66ab78a0b765d06102d4ab55a8cf861ec18
xoxoedg <rugyasin@gmail.com>
2021-11-13 22:24:04 +0100

    Added static recourses

--- pet-clinic-data/src/main/java/com/example/rug/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/rug/services/OwnerService.java
@@ -4,7 +4,7 @@ import com.example.rug.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService extends  CrudService<Owner, Long> {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
