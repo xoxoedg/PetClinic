b8a865162e9d2a102ea3cc3d68c64c640a283211
xoxoedg <rugyasin@gmail.com>
2021-11-13 12:21:00 +0100

    MapService

--- pet-clinic-data/src/main/java/com/example/rug/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/rug/services/map/PetServiceMap.java
@@ -26,7 +26,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 
     @Override
     public Pet save(Pet object) {
-        return this.save(object.getId(), object);
+        return this.save(object);
     }
 
     @Override
