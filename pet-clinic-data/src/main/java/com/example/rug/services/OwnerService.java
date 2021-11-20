47738e86b100fcc56d0a56d1db4b5fe69e59f566
xoxoedg <rugyasin@gmail.com>
2021-11-20 21:43:36 +0100

    Added Test dependencies

--- pet-clinic-data/src/main/java/com/example/rug/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/rug/services/OwnerService.java
@@ -2,8 +2,6 @@ package com.example.rug.services;
 
 import com.example.rug.model.Owner;
 
-import java.util.Set;
-
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
--- pet-clinic-web/src/test/java/rug/RugApplicationTests.java
+++ pet-clinic-web/src/test/java/rug/RugApplicationTests.java
@@ -2,8 +2,13 @@ package rug;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@SpringBootTest(classes = SpringExtension.class)
 class RugApplicationTests {
 
+    @Test
+    void name() {
+    }
 }
