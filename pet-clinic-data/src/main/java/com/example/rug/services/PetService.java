bd903e23a38bb3ce58a8b44852098189c62da3df
xoxoedg <rugyasin@gmail.com>
2021-11-21 16:39:18 +0100

    test pom commit

--- pet-clinic-data/src/main/java/com/example/rug/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/rug/services/PetService.java
@@ -1,9 +1,6 @@
 package com.example.rug.services;
 
-import com.example.rug.model.Owner;
 import com.example.rug.model.Pet;
-
-import java.util.Set;
 
 public interface PetService extends  CrudService<Pet, Long> {
 }
