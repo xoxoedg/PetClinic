da586fb9feb8da24749bd8a7d494ed351ca862d5
xoxoedg <rugyasin@gmail.com>
2021-11-23 23:54:38 +0100

    Added Tests to PetServiceMap

--- pet-clinic-data/src/test/java/com/example/rug/services/map/PetServiceMapTest.java
+++ pet-clinic-data/src/test/java/com/example/rug/services/map/PetServiceMapTest.java
@@ -1,36 +1,62 @@
 package com.example.rug.services.map;
 
-import com.example.rug.repositories.PetTypeRepository;
+import com.example.rug.model.Owner;
+import com.example.rug.model.Pet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.Set;
 
 class PetServiceMapTest {
-
-    PetTypeRepository ownerServiceMap;
+    final Long id = 1L;
+    PetServiceMap petServiceMap;
 
     @BeforeEach
     void setUp() {
-//        ownerServiceMap = new OwnerServiceMap(new PetTypeMapService(), )
+        petServiceMap = new PetServiceMap();
+        petServiceMap.save(Pet.builder()
+                .name("Freddy")
+                .owner(Owner.builder()
+                        .id(id)
+                        .lastName("Walter")
+                        .build())
+                .birthDate(LocalDate.now())
+                .build());
     }
 
     @Test
     void findAll() {
+        Set<Pet> pets = petServiceMap.findAll();
+        assertEquals(1, pets.size());
+        pets.add(new Pet());
+        assertEquals(2, pets.size());
 
     }
 
     @Test
     void deleteById() {
+        petServiceMap.deleteById(id);
+        assertEquals(0, petServiceMap.findAll().size());
+
     }
 
     @Test
     void delete() {
+        petServiceMap.delete(petServiceMap.findById(id));
+        assertNull(petServiceMap.findById(id));
     }
 
     @Test
     void save() {
+        petServiceMap.save(petServiceMap.findById(id));
+        assertNotNull(petServiceMap.findById(id));
+        assertNotNull(petServiceMap.findAll());
     }
 
     @Test
     void findById() {
+        petServiceMap.save(petServiceMap.findById(id));
+        assertNotNull(petServiceMap.findById(id));
     }
 }
